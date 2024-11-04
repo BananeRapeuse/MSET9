3db3aff39379ee260bc4859a9b311370855f1eb2
Ph0qu3_111 <79283690+BananeRapeuse@users.noreply.github.com>
2024-11-04 18:22:24 +0100

    Update MSET9-Windows.bat

--- MSET9_installer_script/MSET9-Windows.bat
+++ MSET9_installer_script/MSET9-Windows.bat
@@ -2,8 +2,8 @@
 chcp 65001 > nul
 py -V > nul
 if %errorlevel% NEQ 0 (
-    echo Python 3 is not installed.
-    echo Please install Python 3 and try again.
+    echo Python 3 n'est pas installé.
+    echo Installez Python et ré-essayez.
     echo https://www.python.org/downloads/
     echo.
     pause
